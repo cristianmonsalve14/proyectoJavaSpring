ae9b391e5389fbd61e60a292ccc06a6b2e7cee2e
Cristian Monsalve <c.m.budrovich@gmail.com>
2023-10-08 23:03:17 -0300

    proyecto java spring

---
+++ java_springboot/src/main/java/cl/polyden/app/JavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package cl.polyden.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootApplication.class, args);
+	}
+
+}
