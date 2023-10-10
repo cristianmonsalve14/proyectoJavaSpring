c5e2d64a3077a3ba1b6f0a12b113e0918e1dca2c
Cristian Monsalve <c.m.budrovich@gmail.com>
2023-10-10 20:56:21 -0300

    proyecto java utlizando spring

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/b7/40700c34c667001e1deef37b30c86042
@@ -0,0 +1,8 @@
+package cl.polyden.app.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class StudentController {
+
+}
--- java_springboot/src/main/java/cl/polyden/app/controller/StudentController.java
+++ java_springboot/src/main/java/cl/polyden/app/controller/StudentController.java
@@ -1,8 +1,14 @@
 package cl.polyden.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StudentController {
+	
+	@GetMapping({"/students", "/"})
+	public String getAllStudents() {
+		return "students";
+	}
 
 }
