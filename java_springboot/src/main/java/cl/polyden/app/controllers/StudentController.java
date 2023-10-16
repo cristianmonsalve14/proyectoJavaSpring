189ca91c2486a8b6065e49f057e2be2218a03910
Cristian Monsalve <c.m.budrovich@gmail.com>
2023-10-15 22:21:50 -0300

    metodos en el controlador y el service listo para crear estudiantes

--- java_springboot/src/main/java/cl/polyden/app/controllers/StudentController.java
+++ java_springboot/src/main/java/cl/polyden/app/controllers/StudentController.java
@@ -1,5 +1,6 @@
 package cl.polyden.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,27 +8,35 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import cl.polyden.app.entitys.Student;
+import cl.polyden.app.services.IStudentService;
 
 
 @Controller
 public class StudentController {
   
+  private final IStudentService service;
   
+  @Autowired
+  public StudentController(IStudentService service) {
+	  this.service = service;
+  }
+	
   @GetMapping({"/students", "/"})
-  public String getAllStudents() {
-    return "students";
+  public String getAllStudents(Model model) {
+	  model.addAttribute("students", service.listAllStudents());
+	  return "students";
   }
  
   @GetMapping({"/students/new"})
   public String createStudentFormString(Model model) {
 	  Student student = new Student();
 	  model.addAttribute("student", student);
-	  
-    return "create-student";
+	  return "create-student";
   }
   
   @PostMapping("/students")
   public String saveStudent(@ModelAttribute("student") Student student) {
+	  service.saveStudent(student);
 	  return "redirect:/students";
   }
 }
